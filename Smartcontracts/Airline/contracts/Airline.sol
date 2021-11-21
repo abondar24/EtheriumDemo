dbb5b0679ec4a4f668b68e6e7850f087a7bcc644
abondar24 <abondar1992@gmail.com>
2021-11-21 18:39:39 +0100

    airlines smart contract update

--- Smartcontracts/Airline/contracts/Airline.sol
+++ Smartcontracts/Airline/contracts/Airline.sol
@@ -4,75 +4,86 @@ contract Airline {
 
     address  chairperson;
 
-    struct details {
-        uint deposit;
-        uint status;
-        uint detailsHash;
+    struct Request {
+        uint id;
+        uint flightId;
+        uint passengerId;
+        uint seatsNum;
+        address dstAirline;
     }
 
-    mapping(address => details) public balanceDetails;
-    mapping(address => uint) membership;
+    struct Response {
+        uint id;
+        bool status;
+        address srcAirline;
+    }
 
-    modifier onlyChairman{
+    mapping(address => uint) public escrows;
+    mapping(address => uint) memberships;
+    mapping(address => uint) settledRequestIds;
+    mapping(address => Request) requests;
+    mapping(address => Response) responses;
+
+
+    modifier onlyChairperson{
         require(msg.sender == chairperson);
         _;
     }
 
     modifier onlyMember{
-        require(membership[msg.sender] == 1);
+        require(memberships[msg.sender] == 1);
         _;
     }
 
     constructor() public payable {
         chairperson = msg.sender;
-        membership[msg.sender] = 1;
-        balanceDetails[msg.sender].deposit = msg.value;
+        memberships[msg.sender] = 1;
+        escrows[msg.sender] = msg.value;
     }
 
 
     function register() public payable {
-        address airlineFirst = msg.sender;
-        membership[airlineFirst] = 1;
-        balanceDetails[msg.sender].deposit = msg.value;
+        address airline = msg.sender;
+        memberships[airline] = 1;
+        escrows[airline] = msg.value;
     }
 
-    function unregister(address payable airlineLast) public payable {
-        if (chairperson != msg.sender) {
+    function unregister(address payable airline) public onlyChairperson {
+
+        memberships[airline] = 0;
+        airline.transfer(escrows[airline]);
+        escrows[airline] = 0;
+    }
+
+    function request(uint requestId, uint flightId, uint passengerId, uint seatsNum, address dstAirline) onlyMember public {
+        require(memberships[dstAirline] == 1);
+
+        requests[msg.sender] = Request(requestId, flightId, passengerId, seatsNum, dstAirline);
+
+    }
+
+    function response(address srcAirline, uint requestId, bool done) onlyMember public {
+        if (memberships[srcAirline] != 1) {
             revert();
         }
 
-        membership[airlineLast] = 0;
-        airlineLast.transfer(balanceDetails[airlineLast].deposit);
-        balanceDetails[airlineLast].deposit = 0;
+        responses[msg.sender].status = done;
+        responses[msg.sender].srcAirline = srcAirline;
+        responses[msg.sender].id = requestId;
     }
 
-    function request(address destAirline, uint detailsHash) onlyMember public {
-        if (membership[destAirline] != 1) {
-            revert();
-        }
-
-        balanceDetails[msg.sender].status = 0;
-        balanceDetails[msg.sender].detailsHash = detailsHash;
-
-    }
-
-    function response(address srcAirline, uint detailsHash, uint done) onlyMember public {
-        if (membership[srcAirline] != 1) {
-            revert();
-        }
-
-        balanceDetails[msg.sender].status = done;
-        balanceDetails[srcAirline].detailsHash = detailsHash;
-    }
-
-    function settlePayment(address payable destAirline) onlyMember payable public {
+    function settlePayment(uint requestId, uint seatsNum, address payable dstAirline) onlyMember payable public {
         address srcAirline = msg.sender;
-        uint amount = msg.value;
-        balanceDetails[destAirline].deposit = balanceDetails[destAirline].deposit + amount;
+        uint amount = 500000000;
+        escrows[dstAirline] = escrows[dstAirline] + amount * seatsNum;
 
-        balanceDetails[srcAirline].deposit = balanceDetails[srcAirline].deposit - amount;
+        escrows[srcAirline] = escrows[srcAirline] - amount * seatsNum;
 
-        destAirline.transfer(amount);
+        settledRequestIds[srcAirline] = requestId;
+    }
+
+    function replenishEscrow() payable public {
+        escrows[msg.sender] = escrows[msg.sender] + msg.value;
     }
 
 
